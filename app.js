92074d40aa5a4fa84e9447c436ed14893d499604
staceyanne <staceywhite10@gmail.com>
2023-03-30 18:22:45 +0100

    ammended file for deployment

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/api/reviews', getAllReviews)
 app.get('/api/reviews/:review_id/comments', getCommentsByReviewId)
 app.post('/api/reviews/:review_id/comments', postCommentByReviewId)
 
-app.all('/*', (req, res, next) => {
+app.use('/*', (req, res, next) => {
     res.status(404).send({ msg: "Incorrect file path"})
     next(err)
 })
