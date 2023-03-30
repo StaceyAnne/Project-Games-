a6bc33b36bfc87e43be3004175c78c7ab5946e9b
staceyanne <staceywhite10@gmail.com>
2023-03-30 16:59:57 +0100

    updated comments.models

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,6 +1,6 @@
 const db = require('../db/connection')
 const { fetchReview } =  require('./reviews.models')
-const format = require('pg-format')
+
 
 
 exports.fetchCommentsByReviewId = (reviewId) => {
