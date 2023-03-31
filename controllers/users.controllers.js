11974203fa108ddda128447f79f9221da0e13699
Stacey <staceywhite10@gmail.com>
2023-03-31 11:13:59 +0100

    Revert "Ticket10"

--- controllers/users.controllers.js
+++
@@ -1,8 +0,0 @@
-const { fetchUsers } = require('../models/users.model');
-
-
-exports.getUsers = (req,res,next) => {
-    return fetchUsers().then(({ rows }) => {
-        res.status(200).send( { rows })
-    }).catch(next)
-}
--- models/users.model.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db/connection')
-
-exports.fetchUsers = () => {
-    const queryString = `SELECT * FROM users;`
-   return db.query(queryString).then((users) => {
-    return users; 
-})
-}
